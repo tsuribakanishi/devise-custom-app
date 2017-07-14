5cd177d949f242a5615f8a8b2b8a291fc21820d3
tsuribakanishi <tsuribakanishi.work@gmail.com>
2017-07-14 23:30:39 +0900

    人気の本　件数条件追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,8 @@ class BooksController < ApplicationController
     @book = Book.search
     strSql = "select book_id,count(book_id) as yoyaku_su from tickets
               group by book_id
-              order by count(book_id) desc"
+              order by count(book_id) desc
+              limit 5"
     @popularbooks = Book.find_by_sql([strSql])
   end
 
@@ -82,7 +83,8 @@ class BooksController < ApplicationController
 
     strSql = "select book_id,count(book_id) as yoyaku_su from tickets
               group by book_id
-              order by count(book_id) desc"
+              order by count(book_id) desc
+              limit 5"
     @popularbooks = Book.find_by_sql([strSql])
 
 
